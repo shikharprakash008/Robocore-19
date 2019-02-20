da3645851db2a1ae5afe8f8d69e4f7f2436845b4
shikharprakash008 <47673757+shikharprakash008@users.noreply.github.com>
2019-02-20 21:23:23 +0530

    first commit

---
+++ app/src/main/java/surfer/example/surfer/robobore/Team.java
@@ -0,0 +1,43 @@
+package surfer.example.surfer.robobore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class Team extends AppCompatActivity {
+  ImageView m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11,m12,m13,m14,m15,m16,m17,m18,m19,m20,m21,m22,m23,m24,m25,m26,m27,m28,m29,m30;
+  ImageView m31,m32,m33,m34,m35,m36,m37,m38,m39,m40,m41,m42,m43,m44,m45,m46,m47,m48,m49,m50,m51,m52,m53;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_team);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
