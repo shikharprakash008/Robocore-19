9a23507b440ab2bfd996680e1d8aef23bd3a1569
shikharprakash008 <47673757+shikharprakash008@users.noreply.github.com>
2019-02-21 15:47:09 +0530

    mainpage commit

--- app/src/main/java/surfer/example/surfer/robobore/MainActivity.java
+++ app/src/main/java/surfer/example/surfer/robobore/MainActivity.java
@@ -8,6 +8,7 @@ import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -19,10 +20,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.animation.LinearInterpolator;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+
+
 
 
 
